b4ac9483228f35b9b9afe9d0773a81688e9440c1
K Ramesh Babu <165985838+kademrameshbabu@users.noreply.github.com>
2026-04-17 21:52:18 +0530

    Update index.js

--- express_server/index.js
+++ express_server/index.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const express = require("express");
 const ioredis = require('ioredis');
-const { get_access_token, set_access_token, coupons_valid_for_basket, redeem_coupons, rollback_coupons, configure_api_client, set_redis_client } = require("../index");
+const { get_access_token, set_access_token, coupons_valid_for_basket, redeem_coupons, rollback_coupons, configure_api_client, set_redis_client } = require("pos-validation-sdk");
 const app = express();
 
 const tcb_endpoint = process.env.TBC_ENDPOINT;
@@ -19,7 +19,6 @@ let redisConnObj = {
 const redisClient = new ioredis(redisConnObj);
 
 function return_error_response(res, error) {
-    console.log(error);
     if (error.response) {
         delete error.response.data.execution_start_time;
         delete error.response.data.execution_time_in_ms;
@@ -43,7 +42,13 @@ app.post("/coupons_valid_for_basket", async (req, res) => {
     try {
         const start_time = performance.now();
         const input = req.body;
-        const { retailer_email_domain, access_key, access_token, use_redis } = req.headers;
+        //const { retailer_email_domain, access_key, access_token, use_redis } = req.headers;
+        // 🔥 FIXED HEADER EXTRACTION
+        const retailer_email_domain = req.headers["x-retailer-domain"];
+        const access_key = req.headers["x-api-key"];
+        const access_token = req.headers["x-access-token"];
+        const use_redis = req.headers["use_redis"];
+
         await configure_api_client(tcb_endpoint, 10000, 3, 1000);
         set_access_token(access_key, access_token);
         if ( use_redis ) set_redis_client(redisClient);
@@ -59,7 +64,11 @@ app.post("/redeem_coupons", async (req, res) => {
     try {
         const start_time = performance.now();
         const { coupons, retailer_email_domain } = req.body;
-        const { access_key, access_token, use_redis } = req.headers;
+        //const { access_key, access_token, use_redis } = req.headers;
+        const access_key = req.headers["x-api-key"];
+        const access_token = req.headers["x-access-token"];
+        const use_redis = req.headers["use_redis"];
+
         await configure_api_client(tcb_endpoint, 10000, 3, 1000);
         set_access_token(access_key, access_token);
         if ( use_redis ) set_redis_client(redisClient);
@@ -75,7 +84,10 @@ app.post("/rollback_coupons", async (req, res) => {
     try {
         const start_time = performance.now();
         const { coupons, retailer_email_domain } = req.body;
-        const { access_key, access_token } = req.headers;
+        //const { access_key, access_token } = req.headers;
+        const access_key = req.headers["x-api-key"];
+        const access_token = req.headers["x-access-token"];     
+
         await configure_api_client(tcb_endpoint, 10000, 3, 1000);
         set_access_token(access_key, access_token);
         const rolled_back_coupons = await rollback_coupons(coupons, retailer_email_domain);
